2fab25624f221fd6b1540a0ea639f1dfcedc8d9a
Alessandro Ledda <aleledda75@icloud.com>
2026-02-09 19:22:17 +0100

    feat in file controllers.js (aggiunta logiche)

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -5,12 +5,57 @@ const blogList = require('./../data/posts');
 
 // index
 function index(req, res) {
-    res.send("siamo nella logica dell'index")
+
+    // risposta in formato json di  tutto l'arrey 
+    // res.json(blogList)
+
+    // inizialmente, il menu filtrato corrisponde a quello originale
+    let filteredList = blogList;
+
+    // se la richiesta contiene un filtro, allora filtriamo
+    if (req.query.dolci) {
+        filteredList = blogList.filter(
+            blog => blog.dolci.includes(req.query.dolci)
+        );
+    }
+
+    // creo oggetto menu con le prop che servono
+    const objList = {
+        numeroBlog: filteredList.length,
+        listaBlog: filteredList
+    }
+
+    // risposta in formato json
+    res.json(objList);
 }
+
+
 
 // show
 function show(req, res) {
-    res.send("siamo nella logica dello show")
+
+    // recupero id dall'url del blog corrente e trasformarlo in un numero
+    const idNum = parseInt(req.params.id)
+
+    // cerchiamo la pizza in base al suo id di ref
+    const blog = blogList.find(blog => blog.id === idNum);
+
+    // condione di check se trovato item
+    if (!blog) {
+
+        // forziamo il messaggio d'errore a 404
+        res.status(404);
+
+        // res con oggetto di errore
+        return res.json({
+            error: "not found",
+            messagge: "blog non trovato"
+
+        })
+    }
+
+    // restituiamo il tutto in formato json
+    res.json(blog);
 }
 
 // store
@@ -30,6 +75,30 @@ function modify(req, res) {
 
 // destroy
 function destroy(req, res) {
+
+    // recupero id dall'url del blog corrente e trasformarlo in un numero
+    const id = parseInt(req.params.id)
+
+    // cerchaimo il blog da eliminare per id
+    const blog = blogList.find(blog => blog === id);
+
+    // controllo per eventuali errori
+    if (!blog) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "not found",
+            message: "blog non trovato"
+        })
+    }
+
+    // rimuoviamo la pizza dal menu
+    blogList.splice(blogList.indexOf(blog), 1);
+
+    // forzo status del successo della cancellazione (204)
+    res.sendStatus(204)
+
     res.send("siamo nella logica del destroy")
 }
 
