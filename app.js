994f6b29ac4f6d65c045b80993ec2388f474dc94
Alessandro Ledda <aleledda75@icloud.com>
2026-02-11 13:06:09 +0100

    feat in file checkTime.js + import e registrazione  in file app.js + test in console

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+//  import del middleware checkTime
+const checkTime = require('./middlewares/checkTime');
+
 // importazione router dei blog
 const postsRouter = require('./routers/posts');
 
@@ -12,10 +15,16 @@ app.use(express.static('public'));
 // registro body-parser per application/json
 app.use(express.json());
 
+// registro middleware checkTime
+app.use(checkTime);
+
 // rotta per la home app
 app.get('/', (req, res) => {
     res.send("rotta di home della nostra app di blog")
 })
+
+// registrazione middleware su router specifico
+app.use("/posts", checkTime)
 
 // istanza delle rotte per risorsa blog
 app.use("/posts", postsRouter)
