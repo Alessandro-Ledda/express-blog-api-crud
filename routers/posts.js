f9aa9746ebff34e4eacafc264b5a4bf16df44971
Alessandro Ledda <aleledda75@icloud.com>
2026-02-11 13:22:40 +0100

    fix route

--- routers/posts.js
+++ routers/posts.js
@@ -4,13 +4,19 @@ const express = require('express');
 // creiamo un istanza dell'oggetto rotte di express
 const router = express.Router();
 
+//  import del middleware checkTime
+const checkTime = require('./../middlewares/checkTime');
+
+// registrato per le rotte di questo router
+router.use(checkTime);
+
 // import del controller della risorsa blog
 const postsController = require('./../controllers/postsControllers');
 
 // rotte crud per i blog
 
 // index
-router.get('/', postsController.index);
+router.get('/', checkTime, postsController.index);
 
 // show
 router.get('/:id', postsController.show);
@@ -25,7 +31,7 @@ router.put('/:id', postsController.update);
 router.patch('/:id', postsController.modify);
 
 // destroy
-router.delete('/:id', postsController.destroy);
+router.delete('/:id', checkTime, postsController.destroy);
 
 // esporto l'istanza delle rotte
 module.exports = router;
